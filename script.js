6f4835f4e4cfc4be1d21ad74ca72338cbea195a4
carolina <ci.arayagonzalez@gmail.com>
2024-02-19 09:43:45 -0300

    :sparkles: Add ship in board

--- script.js
+++ script.js
@@ -24,16 +24,25 @@ let ship = {
     height : shipHeight
 }
 
+let shipImage;
+
 window.onload = function(){
+
     board = document.getElementById('board');
     board.width = boardWidth;
     board.height = boardHeight;
     context = board.getContext('2d');//se usa para dibujar en eltablero
 
     //draw ship
-    context.fillStyle = 'green';
-    context.fillRect(ship.x, ship.y, shipWidth, shipHeight);
+    // context.fillStyle = 'green';
+    // context.fillRect(ship.x, ship.y, shipWidth, shipHeight);
 
+    let shipImage = new Image();
+    shipImage.src = '/assets/ship.png';
+    
+    shipImage.onload = function(){
+        context.drawImage(shipImage, ship.x, ship.y, ship.width, ship.height);
+    }
 }
 
 
