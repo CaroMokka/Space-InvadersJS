754511224e694d9cbbfa0f63929009a508927a6f
carolina <ci.arayagonzalez@gmail.com>
2024-02-18 22:43:48 -0300

    :sparkles: Initial ship + board

---
+++ script.js
@@ -0,0 +1,41 @@
+//board
+let tileSize = 32;
+let rows = 16;
+let cols = 16;
+
+let boardWidth = tileSize * cols;
+let boardHeight = tileSize * rows;
+
+let board;
+
+let context;
+
+//ship
+let shipWidth = tileSize * 2;
+let shipHeight = tileSize;
+
+let shipX = tileSize * cols/2 - tileSize;
+let shipY = tileSize * rows - tileSize*2;
+
+let ship = {
+    x : shipX,
+    y : shipY,
+    width : shipWidth,
+    height : shipHeight
+}
+
+window.onload = function(){
+    board = document.getElementById('board');
+    board.width = boardWidth;
+    board.height = boardHeight;
+    context = board.getContext('2d');//se usa para dibujar en eltablero
+
+    //draw ship
+    context.fillStyle = 'green';
+    context.fillRect(ship.x, ship.y, shipWidth, shipHeight);
+
+}
+
+
+
+
