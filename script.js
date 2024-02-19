159ba39521be578b01ada422d9306c1316fab1c8
carolina <ci.arayagonzalez@gmail.com>
2024-02-19 12:05:01 -0300

    :sparkles: Show aliens in the board

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ let context;
 let shipWidth = tileSize * 2;
 let shipHeight = tileSize;
 
-let shipX = tileSize * cols/2 - tileSize;
-let shipY = tileSize * rows - tileSize*2;
+let shipX = (tileSize * cols) / 2 - tileSize;
+let shipY = tileSize * rows - tileSize * 2;
 
 let ship = {
-    x : shipX,
-    y : shipY,
-    width : shipWidth,
-    height : shipHeight
-}
+  x: shipX,
+  y: shipY,
+  width: shipWidth,
+  height: shipHeight,
+};
 
 let shipImage;
 let shipMovingX = tileSize;
@@ -34,42 +34,75 @@ let alienHeight = tileSize;
 let alienX = tileSize;
 let alienY = tileSize;
 
-window.onload = function(){
+let alienImage;
+let aliensRows = 2;
+let aliensCols = 3;
+let aliensCount = 0;
 
-    board = document.getElementById('board');
-    board.width = boardWidth;
-    board.height = boardHeight;
-    context = board.getContext('2d');//se usa para dibujar en eltablero
+window.onload = function () {
+  board = document.getElementById("board");
+  board.width = boardWidth;
+  board.height = boardHeight;
+  context = board.getContext("2d"); //se usa para dibujar en eltablero
 
-    //draw ship
-    // context.fillStyle = 'green';
-    // context.fillRect(ship.x, ship.y, shipWidth, shipHeight);
+  //draw ship
+  // context.fillStyle = 'green';
+  // context.fillRect(ship.x, ship.y, shipWidth, shipHeight);
 
-    shipImage = new Image();
-    shipImage.src = '/assets/ship.png';
-    
-    shipImage.onload = function(){
-       
-        context.drawImage(shipImage, ship.x, ship.y, ship.width, ship.height);
-    }
-    requestAnimationFrame(update);
-    document.addEventListener("keydown", moveShip)
-}
+  shipImage = new Image();
+  shipImage.src = "/assets/ship.png";
 
-function update(){
-    requestAnimationFrame(update);
-    context.clearRect(0, 0, board.width, board.height);
-    //ship una y otra vez, genera animacion
-    //console.log(typeof shipImage[this.spriteCostumeCount])
+  alienImage = new Image();
+  alienImage.src = "/assets/alien.png";
+  createAliens();
+
+  shipImage.onload = function () {
     context.drawImage(shipImage, ship.x, ship.y, ship.width, ship.height);
+  };
+  requestAnimationFrame(update);
+  document.addEventListener("keydown", moveShip);
+};
+
+function update() {
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
+  //console.log(typeof shipImage[this.spriteCostumeCount])
+  //ship
+  context.drawImage(shipImage, ship.x, ship.y, ship.width, ship.height);
+  //aliens
+//   for(let i = 0; i < aliensArray.length; i++) {
+//     let alien = aliensArray[i];
+//   }
+
+
 }
 
-function moveShip(e){
-    console.log(e);
-    if(e.code == "ArrowLeft" && ship.x - shipMovingX >= 0) {
-        ship.x = ship.x - shipMovingX;
-    } else if(e.code == "ArrowRight" && ship.x + shipMovingX + ship.width <= boardWidth) {
-        ship.x = ship.x + shipMovingX;
+function moveShip(e) {
+  console.log(e);
+  if (e.code == "ArrowLeft" && ship.x - shipMovingX >= 0) {
+    ship.x = ship.x - shipMovingX;
+  } else if (
+    e.code == "ArrowRight" &&
+    ship.x + shipMovingX + ship.width <= boardWidth
+  ) {
+    ship.x = ship.x + shipMovingX;
+  }
+}
+
+function createAliens() {
+  for (let c = 0; c < aliensCols; c++) {
+    for (let r = 0; r < aliensRows; r++) {
+      let alien = {
+        img: alienImage,
+        x: alienX + c*alienWidth,
+        y: alienY + r*alienHeight,
+        width: alienWidth,
+        height: alienHeight,
+        alive : true
+      };
+      aliensArray.push(alien);
     }
+  }
+  aliensCount = aliensArray.length;
 }
-
+ 
